eaf622dc0b483a2d97c96b18389832733e27e446
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2015-12-21 19:07:53 +0000

    Añadiendo prueba de periodico

--- spec/refapa_spec.rb
+++ spec/refapa_spec.rb
@@ -10,11 +10,14 @@ describe RefAPA::Refapa do
             edicion     1
             volumen     1
         end
-        
-        
+        @periodico1 = RefBiblio::ArtPeriodico.new () do
+            autor       ["Rafael Herrero","Daniel Ramos"]
+            titulo      "La regeneracion de las gemas"
+            editorial   "El Mundo"
+            publicacion Date.new(2015,11,17)
+            paginas     130
         
 =begin
-		@periodico1 = RefBiblio::ArtPeriodico.new(["Rafael Herrero","Daniel Ramos"], "La regeneracion de las gemas", Date.new(2015,11,17), "El Mundo", 130)
 		@documento1 = RefBiblio::DocElectronico.new(["Rafael Herrero","Daniel Ramos"], "Estudio del habitat de las gemas", "Universidad de La Laguna", "Oficina de Software Libre de la ULL", Date.new(2015,11,17), "PDF", "http://osl.ull.es/noticias/", Date.new(2015,12,9))
 =end
     end
@@ -26,11 +29,12 @@ describe RefAPA::Refapa do
             @APA.insertar(@libro1)
             expect(@APA.to_s).to eq("11, A. & Ros, M. & Cheeto, S. (January 1, 2015). Salseo Gamer (1) (1). Temas de hoy.\n")
         end
-=begin
+
         it "Insertar periodico y bien formateado" do
             @APA.insertar(@periodico1)
             expect(@APA.to_s).to eq("Herrero, R. & Ramos, D. (November 17, 2015). La Regeneracion de las Gemas. El Mundo, pp. 130.\n")
         end
+=begin
         it "Insertar documento electronico" do
             @APA.insertar(@documento1)
             expect(@APA.to_s).to eq("Herrero, R. & Ramos, D. (November 17, 2015). Estudio del Habitat de las GemasPdf. Universidad de La Laguna: Oficina de Software Libre de la ULL. Disponible en: http://osl.ull.es/noticias/ (December 9, 2015). \n")
