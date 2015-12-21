92ac7a31c81481510bf1a627a849c7b670b7e67d
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2015-12-21 22:13:58 +0000

    Añadida prueba de lista

--- spec/refapa_spec.rb
+++ spec/refapa_spec.rb
@@ -45,13 +45,12 @@ describe RefAPA::Refapa do
             @APA.insertar(@documento1)
             expect(@APA.to_s).to eq("Herrero, R. & Ramos, D. (November 17, 2015). Estudio del Habitat de las GemasPdf. Universidad de La Laguna: Oficina de Software Libre de la ULL. Disponible en: http://osl.ull.es/noticias/ (December 9, 2015). \n")
         end
-=begin
+
         it "Insertar todo y bien formateado" do
         @APA.insertar(@libro1)
         @APA.insertar(@periodico1)
         @APA.insertar(@documento1)
-        expect(@APA.to_s).to eq("11, A. & Ros, M. & Cheeto, S. (January 1, 2015). Salseo Gamer (1) (1). Temas de hoy.\nHerrero, R. & Ramos, D. (November 17, 2015). La Regeneracion de las Gemas. El Mundo, pp. 130.\nHerrero, R. & Ramos, D. (November 17, 2015). Estudio del Habitat de las GemasPdf. Universidad de La Laguna: Oficina de Software Libre de la ULL. Disponible en: http://osl.ull.es/noticias/ (December 9, 2015). \n")
+        expect(@APA.to_s).to eq("11, A. & Ros, M. & Cheeto, S. (January 1, 2015). Salseo Gamer (1) (1). Temas de hoy.\nHerrero, R. & Ramos, D. (November 17, 2015). La Regeneracion de las Gemas. El Mundo, pp. Papel, 130 paginas.\nHerrero, R. & Ramos, D. (November 17, 2015). Estudio del Habitat de las GemasPdf. Universidad de La Laguna: Oficina de Software Libre de la ULL. Disponible en: http://osl.ull.es/noticias/ (December 9, 2015). \n")
         end
-=end
     end
 end
